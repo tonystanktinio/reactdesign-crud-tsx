ab0e9b6856e44862cd0d4ffd63636a0e18961b52
tonystanktinio <timmybrayantbente4@gmail.com>
2021-06-22 11:15:49 +0800

    hey

---
+++ src/store/reducers/notificationReducer.ts
@@ -0,0 +1,27 @@
+import {
+  NotificationState,
+  NotificationAction,
+  SET_NOTIFICATION,
+} from "../types";
+
+const initialState: NotificationState = {
+  message: "",
+  type: "success",
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (
+  state = initialState,
+  action: NotificationAction
+): NotificationState => {
+  switch (action.type) {
+    case SET_NOTIFICATION:
+      return {
+        message: action.payload.msg,
+        type: action.payload.type,
+      };
+
+    default:
+      return state;
+  }
+};
